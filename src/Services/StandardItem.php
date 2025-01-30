b968d9fa5d1e790052630bbb6b337faa5dcb0205
Krystian Szacho <contact@krystian.site>
2025-01-30 22:38:56 +0100

    [Task-6]: Clean code

--- src/Services/StandardItem.php
+++ src/Services/StandardItem.php
@@ -8,6 +8,6 @@ class StandardItem extends ItemBase
     {
         $qualityDecrease = ($this->item->sell_in <= 0) ? 2 : 1;
         $this->item->quality = max(0, $this->item->quality - $qualityDecrease);
-        $this->item->sell_in--; // Dopiero po obniżeniu jakości zmniejszamy sell_in
+        $this->item->sell_in--;
     }
 }
