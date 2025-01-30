57166575fa275d5684450eba8c472f4878ede576
Krystian Szacho <contact@krystian.site>
2025-01-30 20:37:26 +0100

    [Task-3]: Add GlidedRose Services

---
+++ src/Services/GlidedRose.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Item;
+
+final class GildedRose
+{
+    private array $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public function updateQuality(): void
+    {
+        array_walk($this->items, function ($item) {
+            $this->getItemHandler($item)->update();
+        });
+    }
+
+    private function getItemHandler(Item $item): ItemBase
+    {
+        return match ($item->name) {
+            'Aged Brie' => new AgedBrie($item),
+            'Backstage passes to a TAFKAL80ETC concert' => new BackstagePass($item),
+            'Sulfuras, Hand of Ragnaros' => new Sulfuras($item),
+            default => new NormalItem($item),
+        };
+    }
+}
